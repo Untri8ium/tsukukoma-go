586fab5d59a7b2b6f592838dd75b5262e2496084
Untri8ium <19a19u11ek0n@gmail.com>
2025-10-18 13:39:17 +0900

    fix disabled opacity

--- app/page.tsx
+++ app/page.tsx
@@ -254,7 +254,7 @@ export default function HomePage() {
             !destination ||
             currentLocation.locid === destination.locid
           }
-          className="w-full bg-yellow-700 dark:bg-amber-300 text-primary-foreground font-bold italic text-8xl px-6 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed hover:bg-primary/90 transition-colors overflow-visible"
+          className="w-full bg-yellow-700 dark:bg-amber-300 disabled:bg-yellow-700/30 dark:disabled:bg-amber-300/30 text-primary-foreground font-bold italic text-8xl px-6 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed hover:bg-primary/90 transition-colors overflow-visible"
         >
           <span className="-my-4 -ml-2 text-white dark:text-black block">
             GO!
