4cc3024b8be4505dc2a3b0aeaa51bea037a9196f
Untri8ium <19a19u11ek0n@gmail.com>
2025-10-21 10:48:59 +0900

    change metadata title

--- app/page.tsx
+++ app/page.tsx
@@ -33,10 +33,13 @@ export async function generateMetadata({
   let title = "Tsukukoma GO | 筑駒文化祭経路ナビ";
   let description = "急ごう、はしゃごう、Tsukukoma GO!";
   if (depName && destName) {
+    title = `${depName} から ${destName} | Tsukukoma GO`;
     description = `「${depName}」から「${destName}」へ、急ごう、はしゃごう、Tsukukoma GO!`;
   } else if (depName) {
+    title = `${depName} から | Tsukukoma GO`;
     description = `「${depName}」からどこへでも、急ごう、はしゃごう、Tsukukoma GO!`;
   } else if (destName) {
+    title = `${destName} へ | Tsukukoma GO`;
     description = `どこからでも「${destName}」へ、急ごう、はしゃごう、Tsukukoma GO!`;
   }
   return {
